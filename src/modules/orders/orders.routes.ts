a0ab5f783637b225c9621d94cb9989a67ff61cb9
Daniel Henrique <danielhenriqueandrade6@gmail.com>
2026-08-02 19:52:34 -0300

    Add: Ajuste JWT, e segurança nos arquivos PDF

--- src/modules/orders/orders.routes.ts
+++ src/modules/orders/orders.routes.ts
@@ -20,6 +20,10 @@ export async function ordersRoutes(fastify: FastifyInstance) {
     ordersController.listPendingPayment
   )
   fastify.get(
+    '/:id/pdf',
+    ordersController.getPdf
+  )
+  fastify.get(
     '/admin/all',
     { preHandler: adminMiddleware },
     ordersController.listAll
